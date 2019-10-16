ca3fcbf96a11e9a72b65acf55d1e31150c066f24
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-10-16 13:01:51 -0400

    adding notes to variablesExample

---
+++ variablesAssignments.py
@@ -0,0 +1,12 @@
+# Python uses Dynamic Typing.
+# This means you can reassign variables to different data types.
+#example of this would be
+people_count = 2
+print(people_count)
+people_count = ["Jonathan", "Nathalie"]
+print(people_count)
+# end of example
+
+# type() is available to check to see what the current data type is
+print(type(people_count))
+# end of example
