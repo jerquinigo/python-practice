ae8a2907298c4f52a71297f33831eb1fd1da7bc8
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-10-18 13:36:37 -0400

    dictonaries notes'

--- dictionaries.py
+++ dictionaries.py
@@ -19,3 +19,33 @@ dict2 = {
 
 
 print(dict2['admins']['admin1'], "print out nested dictionaries")
+
+
+# can use the power of dictionaries to have nested dictonaries hold lists
+
+# example1
+
+dict3 = {"list1": ["apple", "pear", "peach"]}
+print(dict3['list1'][2])
+# will return peach
+
+# you can add a new key value pair to a dictonary. 
+# example
+
+dict3["list2"] = "groceries for the week"
+print(dict3)
+#{'list1': ['apple', 'pear', 'peach'], 'list2': 'groceries for the week'}
+
+# you can print out either all the keys or all the values of a dictonary with a method. The methods are
+# .keys() and .values()
+
+#example
+
+print(dict3.keys(), "the keys for dict3")
+# (['list1', 'list2'], 'the keys for dict3')
+print(dict3.values(), "the values for dict3")
+#([['apple', 'pear', 'peach'], 'groceries for the week'], 'the values for dict3')
+
+# another method is .items() This allows to show its key value pair in a tuple form
+print(dict3.items(), "item method results")
+# ([('list1', ['apple', 'pear', 'peach']), ('list2', 'groceries for the week')], 'item method results')
